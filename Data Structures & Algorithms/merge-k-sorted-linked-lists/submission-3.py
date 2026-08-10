8039aca38aeaed0705fb399963e1fd196b7c8edb
Firefox-097 <nischay.bharija1729@gmail.com>
2026-08-11 00:22:39 +0530

    Add: merge-k-sorted-linked-lists - submission-3

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-3.py
@@ -0,0 +1,61 @@
+# Definition for singly-linked list.
+#
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+
+        if not lists:
+            return None
+
+        def mergeTwoLists(list1, list2):
+
+            if list1 is None:
+                return list2
+
+            if list2 is None:
+                return list1
+
+            if list1.val <= list2.val:
+                head = list1
+                list1 = list1.next
+            else:
+                head = list2
+                list2 = list2.next
+
+            curr = head
+
+            while list1 is not None and list2 is not None:
+
+                if list1.val <= list2.val:
+                    curr.next = list1
+                    list1 = list1.next
+                else:
+                    curr.next = list2
+                    list2 = list2.next
+
+                curr = curr.next
+
+            if list1 is not None:
+                curr.next = list1
+            else:
+                curr.next = list2
+
+            return head
+
+        def mergeRange(left, right):
+
+            if left == right:
+                return lists[left]
+
+            mid = (left + right) // 2
+
+            firstHalf = mergeRange(left, mid)
+            secondHalf = mergeRange(mid + 1, right)
+
+            return mergeTwoLists(firstHalf, secondHalf)
+
+        return mergeRange(0, len(lists) - 1)
