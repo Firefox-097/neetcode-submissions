1ce55a38683f549578b7ed10608c5361c54f825a
Firefox-097 <nischay.bharija1729@gmail.com>
2026-09-09 21:57:54 +0530

    Add: implement-prefix-tree - submission-2

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-2.cpp
@@ -0,0 +1,44 @@
+class TrieNode{
+    public:
+    TrieNode* child[26];
+    bool isEnd;
+    TrieNode(){
+        for(int i=0;i<26;i++){
+            child[i]=NULL;
+        }
+        isEnd=false;
+    }
+};
+class PrefixTree {
+public:
+    TrieNode* root= new TrieNode();
+    PrefixTree() {      
+    }
+    void insert(string word) {
+        TrieNode* curr=root;
+        for(int i=0;i<word.size();i++){
+            int index=word[i]-'a';
+            if(curr->child[index]==NULL) curr->child[index]= new TrieNode();
+            curr=curr->child[index];
+        }
+        curr->isEnd=true;
+    }
+    bool search(string word) {
+        TrieNode* curr=root;
+        for(int i=0;i<word.size();i++){
+            int index=word[i]-'a';
+            if(curr->child[index]==NULL) return false;
+            curr=curr->child[index];
+        }
+        return curr->isEnd;
+    }
+    bool startsWith(string prefix) {
+        TrieNode* curr=root;
+        for(int i=0;i<prefix.size();i++){
+            int index=prefix[i]-'a';
+            if(curr->child[index]==NULL) return false;
+            curr=curr->child[index];
+        }
+        return true;    
+    }
+};
